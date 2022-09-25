ad81ee5cf2b994589afabaebcaa60bf582750c9f
01000001kuma <a_illera@yahoo.com>
2022-09-25 18:50:57 +0200

    Fetching data

--- src/components/TileBox.tsx
+++ src/components/TileBox.tsx
@@ -1,19 +1,43 @@
-import { Card, Col } from 'antd';
+import React, {useState, useEffect} from "react";
 
-function TileBox({ tile }) {
-  const fetchPost = async () => {
-    const res = await fetch(`http://localhost:3000/api/tiles/${tile.id}`);
+interface Product {
+    _id: string;
+    _fileName: string;
+}
+
+function TileBox(props: any) {
+    const [post, SetPost] = useState<Product[]>([]);
+    const fetchPost = async () => {
+    const res = await fetch(`https://imgmgt.api.preci.cloud/api/SlideImages`);
     const data = await res.json();
-    console.log(data);
-  }
+    SetPost(data)
+
+    if(!res.ok){
+        const msg = `An error has occured: ${res.status}`;
+        throw new Error(msg);
+    }
+
+    };
+
+    useEffect(() => {
+        fetchPost();
+    }, []);
   
     return (
-    <li>
-      <ul>
-        <img src={tile.id} style={{ width: 512, height: 512, margin: 10 }} alt="pics" />
-        <p>{tile.fileName}</p>  
-      </ul>
-    </li>
+    <div className="row">
+        <div className="col-md-12 text-center">
+        </div>
+
+        {post.map(pro=>{
+            return(
+                <div className="col-md-3 text-center">
+                    <img className="w-512" src={pro.id} alt="tileImage" />
+                    <p>{pro.fileName}</p>
+                </div>
+            )
+        })}
+        
+    </div>
   );
 }
 
